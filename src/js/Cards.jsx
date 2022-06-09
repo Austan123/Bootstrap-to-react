1199b7955a3ca19216fb227c25525ecdcc80ec7c
Austan123 <austan.augustin1@gmail.com>
2022-06-09 00:52:48 +0000

    {okay}

---
+++ src/js/Cards.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+const Cards = (props) => {
+  const style = { width: '18rem' }
+
+  return (
+    <div class="d-flex p-3" d-flex justify-content-center>
+      <img src="..." class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+      <div class="card" style={style}>
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+
+
+
+  );
+}
+
+export default Cards;
---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+const Navbar = (props) => {
+return (
+    
+<nav class="navbar bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">React</a>
+  </div>
+</nav>
+
+
+);
+}
+export default Navbar;
+
