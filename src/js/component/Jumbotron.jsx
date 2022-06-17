b9e1c9daf3b57bbbb6bb323f71d251100d0ef124
Austan123 <austan.augustin1@gmail.com>
2022-06-17 19:25:49 +0000

    {Done}

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -4,9 +4,10 @@ const Jumbotron = (props) => {
 
 <div class="p-5 mb-4 bg-light rounded-3">
       <div class="container-fluid py-5">
+      <img src="https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&auto=format&fit=crop&w=1600&q=60" class="card-img-top" alt="..." />
         <h1 class="display-5 fw-bold">Hello World</h1>
         <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-        <button class="btn btn-primary btn-lg" type="button">Example button</button>
+        <button class="btn btn-primary btn-lg" type="button">Continue</button>
       </div>
     </div>
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import Navbar from "./navbar.jsx";
 import Jumbotron from "./Jumbotron.jsx";
-import Cards from "../Cards.jsx";
+import Card from "../Card.jsx";
+import footer from "./Footer.jsx";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Footer from "./Footer.jsx";
+import Cards from "../Card.jsx";
 
 
 //create your first component
 const Home = () => {
+	const myArray = [
+		{ title: "Austan", text: "Like this course", image: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg" },
+		{ title: "Jonas", text: "Lets go!", image: "https://wegotthiscovered.com/wp-content/uploads/2021/10/Two-Marios-640x360.jpg" },
+		{ title: "Paul", text: "I live to race", image: "https://upload.wikimedia.org/wikipedia/commons/b/bc/Bmw_318i_E30_M40_1991_%2851113700%29.jpeg" }
+	]
 	return (
 		<React.Fragment>
-			<Navbar></Navbar>
-			<Jumbotron></Jumbotron>
-			<Cards></Cards>
-			
+			<Navbar />
+			<Jumbotron />
+			<div className="d-flex flex-row">
+				{myArray.map((item, i) => {
+					return (
+						<Cards title={item.title} text={item.text} image={item.image} key={i} />
+					)
+				})}
+
+			</div>
+			<Footer />
+
 		</React.Fragment>
 	);
 };
